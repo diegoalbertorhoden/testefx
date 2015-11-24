243ee045fc7a3a6ed008daf44c356901d2c4ba12
RHODEN <RHODEN@univel.br>
2015-11-24 20:56:22 -0200

    segundo commit pq o primeiro foi michadin....

--- src/application/PrincipalController.java
+++ src/application/PrincipalController.java
@@ -1,5 +1,26 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class PrincipalController {
-	
+
+	@FXML
+	private ResourceBundle resources;
+
+	@FXML
+	private URL location;
+
+	@FXML
+	void salvar(ActionEvent event) {
+
+	}
+
+	@FXML
+	void initialize() {
+
+	}
 }
